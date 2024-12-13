34d08496a5b3d431fe7eb266f610856fe52f79f8
ballalssica <ssicanara@naver.com>
2024-12-13 17:55:20 +0900

    chore: firebase 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tryvel/firebase_options.dart';
 import 'package:tryvel/ui/home/home_page.dart';
 import 'package:tryvel/ui/place_add/place_add_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(ProviderScope(child: const MyApp()));
 }
 
